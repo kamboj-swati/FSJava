868c85194fc9a68746759b990398ab301f73b7e0
Rahat SainiP <rahatsaini@yahoo.com>
2024-03-26 11:07:38 +1100

    day 3 commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public void sumMethod(){
+        int c = 10+20;
+        System.out.println("Sum of numbers is :" +c);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Namaste :");
+        Test sum = new Test();
+        sum.sumMethod();
+    }
+}
